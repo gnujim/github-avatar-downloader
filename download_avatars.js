11b185b2bac20ceb84b6aac30c9a2610bb7dfe22
gnujim <mijung.m@gmail.com>
2017-09-11 20:49:35 +0000

    created requestURL

--- download_avatars.js
+++ download_avatars.js
@@ -1,6 +1,11 @@
 const request = require('request');
+const GITHUB_USER = 'gnujim';
+const GITHUB_TOKEN = '33cae84277b6485a00593a188cd142543e1d8807';
 
-const getRepoContributors = (repoOwner, repoName, cb) => {};
+const getRepoContributors = (repoOwner, repoName, cb) => {
+  const requestURL = `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`;
+  console.log(requestURL);
+};
 
 getRepoContributors('jquery', 'jquery', (err, result) => {
   console.log(`Errors: ${err}`);
