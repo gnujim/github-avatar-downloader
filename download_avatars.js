75c094b60f6b941ff3fb907931d1c4b7967068b8
gnujim <mijung.m@gmail.com>
2017-09-12 00:02:58 +0000

    completed step 5 - making the request

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,35 @@
+require('dotenv').config();
 const request = require('request');
+const fetch = require('node-fetch');
+
 const GITHUB_USER = 'gnujim';
-const GITHUB_TOKEN = '33cae84277b6485a00593a188cd142543e1d8807';
+const GITHUB_TOKEN = process.env.GITHUB_ACCESS_TOKEN;
 
 const getRepoContributors = (repoOwner, repoName, cb) => {
-  const requestURL = `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`;
-  console.log(requestURL);
+  const options = {
+    url: `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`,
+    headers: {
+      'User-Agent': 'GitHub Avatar Downloader - Student Project'
+    }
+  };
+  request
+    .get(options, data => {
+      fetch(options.url)
+        .then(res => {
+          return res.json();
+        })
+        .then(json => {
+          console.log(json);
+        });
+    })
+    .on('error', err => {
+      throw err;
+    })
+    .on('response', response => {
+      console.log(
+        `Response Status Message: ${response.statusCode} ${response.statusMessage}`
+      );
+    });
 };
 
 getRepoContributors('jquery', 'jquery', (err, result) => {
