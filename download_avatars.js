560d3c25f1b2390924b1ac9cfa8520d54b9c3b69
gnujim <mijung.m@gmail.com>
2017-09-12 02:22:20 +0000

    implemented download image by url and connected functions

--- download_avatars.js
+++ download_avatars.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const request = require('request');
-const fetch = require('node-fetch');
+const fs = require('fs');
 
 const GITHUB_USER = 'gnujim';
 const GITHUB_TOKEN = process.env.GITHUB_ACCESS_TOKEN;
@@ -18,10 +18,26 @@ const getRepoContributors = (repoOwner, repoName, cb) => {
       return;
     }
     const data = JSON.parse(body);
-    console.log(data);
+    // console.log(data);
+    cb(error, data);
   });
 };
 
+const downloadImageByURL = (url, filePath) => {
+  request
+    .get(url)
+    // .on('error', err => {
+    //   throw err;
+    // })
+    .pipe(fs.createWriteStream(filePath));
+};
+
 getRepoContributors('jquery', 'jquery', (err, result) => {
-  console.log(`Errors: ${err}`);
+  // console.log(`Errors: ${err}`);
+  // console.log(`Result: ${result}`);
+  result.forEach(user => {
+    let avatarURL = user.avatar_url;
+    let path = `avatars/${user.login}.jpg`;
+    downloadImageByURL(avatarURL, path);
+  });
 });
